1d60c901521f7ddf5d62017422d536728ec63aa8
Zhennan Qin <zhennan.qin@intel.com>
2020-01-07 09:28:58 +0800

    modify it for tf_resnet50

--- tutorials/frontend/from_tensorflow.py
+++ tutorials/frontend/from_tensorflow.py
@@ -42,29 +42,11 @@ except ImportError:
 # Tensorflow utility functions
 import tvm.relay.testing.tf as tf_testing
 
-# Base location for model related files.
-repo_base = 'https://github.com/dmlc/web-data/raw/master/tensorflow/models/InceptionV1/'
-
-# Test image
-img_name = 'elephant-299.jpg'
-image_url = os.path.join(repo_base, img_name)
-
 ######################################################################
 # Tutorials
 # ---------
 # Please refer docs/frontend/tensorflow.md for more details for various models
 # from tensorflow.
-
-model_name = 'classify_image_graph_def-with_shapes.pb'
-model_url = os.path.join(repo_base, model_name)
-
-# Image label map
-map_proto = 'imagenet_2012_challenge_label_map_proto.pbtxt'
-map_proto_url = os.path.join(repo_base, map_proto)
-
-# Human readable text for labels
-label_map = 'imagenet_synset_to_human_label_map.txt'
-label_map_url = os.path.join(repo_base, label_map)
 
 # Target settings
 # Use these commented settings to build for cuda.
@@ -72,26 +54,25 @@ label_map_url = os.path.join(repo_base, label_map)
 #target_host = 'llvm'
 #layout = "NCHW"
 #ctx = tvm.gpu(0)
-target = 'llvm'
+target = 'llvm -mcpu=cascadelake'
 target_host = 'llvm'
 layout = None
 ctx = tvm.cpu(0)
 
 ######################################################################
-# Download required files
-# -----------------------
-# Download files listed above.
-from tvm.contrib.download import download_testdata
-
-img_path = download_testdata(image_url, img_name, module='data')
-model_path = download_testdata(model_url, model_name, module=['tf', 'InceptionV1'])
-map_proto_path = download_testdata(map_proto_url, map_proto, module='data')
-label_path = download_testdata(label_map_url, label_map, module='data')
-
-######################################################################
 # Import model
 # ------------
 # Creates tensorflow graph definition from protobuf file.
+
+model_path = "./fp32_NoBiasAdd_optimized_graph.pb"
+
+# To enable tensorboard log
+
+# graph = tf.get_default_graph()
+# graph_def = graph.as_graph_def()
+# graph_def.ParseFromString(tf_compat_v1.gfile.FastGFile(model_path, 'rb').read())
+# tf.import_graph_def(graph_def, name='graph')
+# summaryWriter = tf.summary.FileWriter('log/', graph)
 
 with tf_compat_v1.gfile.GFile(model_path, 'rb') as f:
     graph_def = tf_compat_v1.GraphDef()
@@ -101,7 +82,7 @@ with tf_compat_v1.gfile.GFile(model_path, 'rb') as f:
     graph_def = tf_testing.ProcessGraphDefParam(graph_def)
     # Add shapes to the graph.
     with tf_compat_v1.Session() as sess:
-        graph_def = tf_testing.AddShapesToGraphDef(sess, 'softmax')
+        graph_def = tf_testing.AddShapesToGraphDef(sess, 'resnet_v2_50/predictions/Reshape_1')
 
 ######################################################################
 # Decode image
@@ -113,10 +94,7 @@ with tf_compat_v1.gfile.GFile(model_path, 'rb') as f:
 #   Hence we supply decoded frame to TVM instead.
 #
 
-from PIL import Image
-image = Image.open(img_path).resize((299, 299))
-
-x = np.array(image)
+x = np.random.uniform(size=(1, 224, 224, 3)).astype('float32')
 
 ######################################################################
 # Import the graph to Relay
@@ -126,8 +104,8 @@ x = np.array(image)
 # Results:
 #   sym: relay expr for given tensorflow protobuf.
 #   params: params converted from tensorflow params (tensor protobuf).
-shape_dict = {'DecodeJpeg/contents': x.shape}
-dtype_dict = {'DecodeJpeg/contents': 'uint8'}
+shape_dict = {'input': x.shape}
+dtype_dict = {'input': 'float32'}
 mod, params = relay.frontend.from_tensorflow(graph_def,
                                              layout=layout,
                                              shape=shape_dict)
@@ -143,10 +121,17 @@ print("Tensorflow protobuf imported to relay frontend.")
 #   params: final params after compilation.
 #   lib: target library which can be deployed on target with TVM runtime.
 
+# enable quantization
+
+with relay.quantize.qconfig(skip_conv_layers=[]):
+    mod = relay.quantize.quantize(mod, params=params)
+
+#print(mod)
+
 with relay.build_config(opt_level=3):
     graph, lib, params = relay.build(mod,
                                      target=target,
-                                     target_host=target_host,
+                                     # target_host=target_host,
                                      params=params)
 
 ######################################################################
@@ -155,85 +140,17 @@ with relay.build_config(opt_level=3):
 # Now we can try deploying the compiled model on target.
 
 from tvm.contrib import graph_runtime
-dtype = 'uint8'
+dtype = 'float32'
 m = graph_runtime.create(graph, lib, ctx)
 # set inputs
-m.set_input('DecodeJpeg/contents', tvm.nd.array(x.astype(dtype)))
+m.set_input('input', tvm.nd.array(x.astype(dtype)))
 m.set_input(**params)
 # execute
 m.run()
-# get outputs
-tvm_output = m.get_output(0, tvm.nd.empty(((1, 1008)), 'float32'))
 
-######################################################################
-# Process the output
-# ------------------
-# Process the model output to human readable text for InceptionV1.
-predictions = tvm_output.asnumpy()
-predictions = np.squeeze(predictions)
-
-# Creates node ID --> English string lookup.
-node_lookup = tf_testing.NodeLookup(label_lookup_path=map_proto_path,
-                                    uid_lookup_path=label_path)
-
-# Print top 5 predictions from TVM output.
-top_k = predictions.argsort()[-5:][::-1]
-for node_id in top_k:
-    human_string = node_lookup.id_to_string(node_id)
-    score = predictions[node_id]
-    print('%s (score = %.5f)' % (human_string, score))
-
-######################################################################
-# Inference on tensorflow
-# -----------------------
-# Run the corresponding model on tensorflow
-
-def create_graph():
-    """Creates a graph from saved GraphDef file and returns a saver."""
-    # Creates graph from saved graph_def.pb.
-    with tf_compat_v1.gfile.GFile(model_path, 'rb') as f:
-        graph_def = tf_compat_v1.GraphDef()
-        graph_def.ParseFromString(f.read())
-        graph = tf.import_graph_def(graph_def, name='')
-        # Call the utility to import the graph definition into default graph.
-        graph_def = tf_testing.ProcessGraphDefParam(graph_def)
-
-def run_inference_on_image(image):
-    """Runs inference on an image.
-
-    Parameters
-    ----------
-    image: String
-        Image file name.
-
-    Returns
-    -------
-        Nothing
-    """
-    if not tf_compat_v1.gfile.Exists(image):
-        tf.logging.fatal('File does not exist %s', image)
-    image_data = tf_compat_v1.gfile.GFile(image, 'rb').read()
-
-    # Creates graph from saved GraphDef.
-    create_graph()
-
-    with tf_compat_v1.Session() as sess:
-        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-        predictions = sess.run(softmax_tensor,
-                               {'DecodeJpeg/contents:0': image_data})
-
-        predictions = np.squeeze(predictions)
-
-        # Creates node ID --> English string lookup.
-        node_lookup = tf_testing.NodeLookup(label_lookup_path=map_proto_path,
-                                            uid_lookup_path=label_path)
-
-        # Print top 5 predictions from tensorflow.
-        top_k = predictions.argsort()[-5:][::-1]
-        print ("===== TENSORFLOW RESULTS =======")
-        for node_id in top_k:
-            human_string = node_lookup.id_to_string(node_id)
-            score = predictions[node_id]
-            print('%s (score = %.5f)' % (human_string, score))
-
-run_inference_on_image(img_path)
+# evaluate
+print("Evaluate inference time cost...")
+ftimer = m.module.time_evaluator("run", ctx, number=100, repeat=3)
+prof_res = np.array(ftimer().results) * 1000  # convert to millisecond
+print("Mean inference time (std dev): %.2f ms (%.2f ms)" %
+        (np.mean(prof_res), np.std(prof_res)))
