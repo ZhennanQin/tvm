9cbc1ba74c20d519e9ecb45c3afbb6728c3ed0b6
Zhennan Qin <zhennan.qin@intel.com>
2020-03-11 09:44:58 +0800

    Add pytorch mobilenetv3

--- tutorials/frontend/from_pytorch.py
+++ tutorials/frontend/from_pytorch.py
@@ -51,40 +51,22 @@ from tvm.relay.frontend.pytorch import get_graph_input_names
 
 # PyTorch imports
 import torch
-import torchvision
+import mobilenetv3
 
 ######################################################################
 # Load a pretrained PyTorch model
 # -------------------------------
-model_name = 'resnet18'
-model = getattr(torchvision.models, model_name)(pretrained=True)
+model = mobilenetv3.mobilenetv3_small()
 model = model.eval()
 
+bs = 128
+
 # We grab the TorchScripted model via tracing
-input_shape = [1, 3, 224, 224]
+input_shape = [bs, 3, 224, 224]
 input_data = torch.randn(input_shape)
 scripted_model = torch.jit.trace(model, input_data).eval()
 
-######################################################################
-# Load a test image
-# -----------------
-# Classic cat example!
-from PIL import Image
-img_url = 'https://github.com/dmlc/mxnet.js/blob/master/data/cat.png?raw=true'
-img_path = download_testdata(img_url, 'cat.png', module='data')
-img = Image.open(img_path).resize((224, 224))
-
-# Preprocess the image and convert to tensor
-from torchvision import transforms
-my_preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
-])
-img = my_preprocess(img)
-img = np.expand_dims(img, 0)
+img = np.random.normal(size=(bs, 3, 224, 224))
 
 ######################################################################
 # Import the graph to Relay
@@ -99,21 +81,18 @@ mod, params = relay.frontend.from_pytorch(scripted_model,
 # Relay Build
 # -----------
 # Compile the graph to llvm target with given input specification.
-target = 'llvm'
-target_host = 'llvm'
-ctx = tvm.cpu(0)
-with relay.build_config(opt_level=3):
-    graph, lib, params = relay.build(mod,
-                                     target=target,
-                                     target_host=target_host,
-                                     params=params)
+target = 'llvm -mcpu=cascadelake'
+with relay.build_config(opt_level=0):
+    graph, lib, params = relay.build(mod, target, params=params)
 
 ######################################################################
 # Execute the portable graph on TVM
 # ---------------------------------
 # Now we can try deploying the compiled model on target.
-from tvm.contrib import graph_runtime
+# from tvm.contrib import graph_runtime
+from tvm.contrib.debugger import debug_runtime as graph_runtime
 dtype = 'float32'
+ctx = tvm.cpu(0)
 m = graph_runtime.create(graph, lib, ctx)
 # Set inputs
 m.set_input(input_name, tvm.nd.array(img.astype(dtype)))
@@ -122,45 +101,11 @@ m.set_input(**params)
 m.run()
 # Get outputs
 tvm_output = m.get_output(0)
+num_iters = 100
+ftimer = m.module.time_evaluator("run", ctx, number=3, repeat=num_iters)
+# prof_res = np.array(ftimer().results) * 1000  # multiply 1000 for converting to millisecond
+speed = bs / (ftimer().mean)
+print("Inference on %d batches, batch size %d, throughput is %f img/sec" % (num_iters, bs, speed))
 
-#####################################################################
-# Look up synset name
-# -------------------
-# Look up prediction top 1 index in 1000 class synset.
-synset_url = ''.join(['https://raw.githubusercontent.com/Cadene/',
-                      'pretrained-models.pytorch/master/data/',
-                      'imagenet_synsets.txt'])
-synset_name = 'imagenet_synsets.txt'
-synset_path = download_testdata(synset_url, synset_name, module='data')
-with open(synset_path) as f:
-    synsets = f.readlines()
-
-synsets = [x.strip() for x in synsets]
-splits = [line.split(' ') for line in synsets]
-key_to_classname = {spl[0]:' '.join(spl[1:]) for spl in splits}
-
-class_url = ''.join(['https://raw.githubusercontent.com/Cadene/',
-                      'pretrained-models.pytorch/master/data/',
-                      'imagenet_classes.txt'])
-class_name = 'imagenet_classes.txt'
-class_path = download_testdata(class_url, class_name, module='data')
-with open(class_path) as f:
-    class_id_to_key = f.readlines()
-
-class_id_to_key = [x.strip() for x in class_id_to_key]
-
-# Get top-1 result for TVM
-top1_tvm = np.argmax(tvm_output.asnumpy()[0])
-tvm_class_key = class_id_to_key[top1_tvm]
-
-# Convert input to PyTorch variable and get PyTorch result for comparison
-with torch.no_grad():
-    torch_img = torch.from_numpy(img)
-    output = model(torch_img)
-
-    # Get top-1 result for PyTorch
-    top1_torch = np.argmax(output.numpy())
-    torch_class_key = class_id_to_key[top1_torch]
-
-print('Relay top-1 id: {}, class name: {}'.format(top1_tvm, key_to_classname[tvm_class_key]))
-print('Torch top-1 id: {}, class name: {}'.format(top1_torch, key_to_classname[torch_class_key]))
+# get outputs
+tvm_output = m.get_output(0)
